69b3b83732b000cfd7a97563c8bccfa8e7039f64
fkzys <fkzys@proton.me>
2026-05-07 13:11:11 +0300

    chore: bump version to 0.8.1

--- lib/common.sh
+++ lib/common.sh
@@ -1,6 +1,6 @@
 # /usr/lib/gitpkg/common.sh — constants, validation, locking, utility helpers
 
-readonly VERSION="0.8.0"
+readonly VERSION="0.8.1"
 readonly DBDIR="/var/lib/gitpkg"
 readonly SRCDIR="/var/cache/gitpkg"
 readonly COLLECTIONSDIR="${SRCDIR}/_collections"
